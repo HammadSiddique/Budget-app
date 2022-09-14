e09ee08255e38da29bba107bb0e44ca0e2ca8c95
HammadSiddique <hammad.siddique01@gmail.com>
2022-09-14 16:39:40 +0500

    Generate devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+         has_many :budgets, dependent: :destroy
+         has_many :categories, through: :category_budgets, dependent: :destroy
+         has_many :category_budgets, through: :categories, dependent: :destroy
+
+         validates :name, presence: true, length: {maximum: 100}
 end
