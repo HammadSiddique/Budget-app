dc2cf9242e7239841606cd2dd88dd97e3f53c625
HammadSiddique <hammad.siddique01@gmail.com>
2022-09-14 21:15:40 +0500

    Generate view for splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/hello'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#splash'
 end
